18dae32f1388f8089fd7d9a31100055aad6cd478
Thiago Sullivan <thiagosullivan12@gmail.com>
2023-03-12 09:51:08 -0300

    Header section

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import WupLogo from '../../../assets/wup-logo.png';
+import { HeaderContainer } from './styles';
+
+function Header() {
+  return (
+    <HeaderContainer>
+        <Link href="/">
+            <Image src={WupLogo} />
+        </Link>
+    </HeaderContainer>
+  )
+}
+
+export default Header
---
+++ src/components/Header/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+    max-width: 1920px;
+    width: 100%;
+    margin: 0 auto;
+    padding: 3.75rem 1rem;
+`;
